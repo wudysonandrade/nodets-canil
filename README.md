42668f36dda8bbc53a809bec5f3ab5b506691575
wudysonandrade <wudyson_sagaz@live.com>
2024-03-25 12:50:14 -0400

    Base Criada

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodets-canil
+
+Projeto feito no modulo do curso Node + typeScript
+
+### Pré-requisitos Globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o Projeto
+`npm run start-dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,23 @@
+import { Request, Response} from 'express';
+
+
+
+export const home = (req: Request, res: Response) => {
+    res.send('home no Controller!');
+    //res.render('pages/page);    
+}
+
+export const dogs = (req: Request, res: Response) => {
+
+    //res.render('pages/dogs);    
+}
+
+export const cats = (req: Request, res: Response) => {
+
+    //res.render('pages/cats);    
+}
+
+export const fishes = (req: Request, res: Response) => {
+
+    //res.render('pages/fishes);    
+}
