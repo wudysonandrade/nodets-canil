a5e207e4c4cb672ddf8fd27bd8df53c2b6431592
wudysonandrade <wudyson_sagaz@live.com>
2024-03-27 11:24:16 -0400

    Final Project 1.0

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send('Pagina não encontrada!');
+    res.render('pages/404');
 });
 
 server.listen(process.env.PORT);
